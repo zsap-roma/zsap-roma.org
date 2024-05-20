cc09a1fe807a61fa7f2d80348d15c4c2f7c6ba45
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-05-20 15:38:02 +0200

    hello ui5 web components

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
+export default defineConfig({
+  base: '/zsap-roma/',
+  integrations: [lit()]
+});

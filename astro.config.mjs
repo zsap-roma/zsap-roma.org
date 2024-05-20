5a344be8f41cb6c0d6fff9a99b04f8d0516f4728
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-05-20 17:17:21 +0200

    astro config?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import lit from "@astrojs/lit";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zsap-roma.github.io',
-  base: 'zsap-roma',
+  base: 'zsap-roma/astro',
   integrations: [lit()],
   output: 'static',
   outDir: './dist',
